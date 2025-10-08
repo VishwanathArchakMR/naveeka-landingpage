a6cb8247635d1a0e53ca9e7e7a090691e0bcb2fe
Vishwanath Archak M R <vishwanath05122006@gmail.com>
2025-10-08 20:43:03 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { HamburgerMenu } from './components/HamburgerMenu';
 import { Hero } from './components/Hero';
 import { VisionStatement } from './components/VisionStatement';
 import { CorePillars } from './components/CorePillars';
@@ -13,15 +14,34 @@ import { Footer } from './components/Footer';
 function App() {
   return (
     <div className="min-h-screen bg-slate-900">
-      <Hero />
-      <VisionStatement />
-      <CorePillars />
-      <RevolutionaryFeatures />
-      <ImpactSection />
-      <MarketOpportunity />
-      <BusinessModel />
-      <FutureImpact />
-      <ClosingSection />
+      <HamburgerMenu />
+      <section id="home">
+        <Hero />
+      </section>
+      <section id="why-now">
+        <VisionStatement />
+      </section>
+      <section id="core-pillars">
+        <CorePillars />
+      </section>
+      <section id="revolutionary-features">
+        <RevolutionaryFeatures />
+      </section>
+      <section id="billion-user-revolution">
+        <ImpactSection />
+      </section>
+      <section id="business-model-market">
+        <MarketOpportunity />
+      </section>
+      <section id="business-model-gtm">
+        <BusinessModel />
+      </section>
+      <section id="responsible-travel">
+        <FutureImpact />
+      </section>
+      <section id="contact">
+        <ClosingSection />
+      </section>
       <Footer />
     </div>
   );
